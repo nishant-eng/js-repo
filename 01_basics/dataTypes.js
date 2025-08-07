33b7cd183c1087c37d817340209ed3cc57960f96
Nishant Pathak <53849975+nishant-eng@users.noreply.github.com>
2025-08-07 08:07:57 +0000

    Changed Datatype and variable

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,32 @@
+// "use Strict" //treat all code as newer version
+// //It is not important to include this
+ 
+// console.log(typeof null) //undefined
+// console.log(typeof undefined) //undefined
+
+
+//Data type Conversion
+let score = "33Nishant"
+let score2 = null  //0 after conversion
+let score3 = undefined
+let valueInNumber = Number(score3)
+// console.log(typeof valueInNumber)  //NaN = Not a Number
+// console.log(valueInNumber)
+
+let marks = 33.55
+let marksinNumber = Number(marks)
+console.log(typeof marksinNumber)
+
+let bool = ""
+let Inboolean =Boolean(bool)
+// console.log(Inboolean)
+//False for empty
+
+
+
+
+//33 = 33
+//"33Nishant" = NaN
+//Boolean => 0,1
+//NULL = 0
+//undefined => NaN
---
+++ 01_basics/variables.js
@@ -0,0 +1,3 @@
+const accountId = 10223
+// accountId = 2 Not allowed
+let accountEmail = "nishant03937@gmail.com"
